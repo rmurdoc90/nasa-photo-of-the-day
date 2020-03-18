32216869a1ab4a183044b91baddb9fecab95a672
Robert Murdoch <rmurdoc90@gmail.com>
2020-03-18 14:26:12 -0700

    2:25pm Added use effects, axios and use state to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState ,useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  const [photo, setPhoto] = useState([]);
+  
+  useEffect(()=>{
+    axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(response =>{
+      console.log(response);
+    })
+    .catch(error => {
+      console.log('Error:', error)
+    })
+  }, [])
+
+  
   return (
     <div className="App">
       <p>
